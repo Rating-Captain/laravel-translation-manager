71c9d6bd26419e26faa9857ee686e952ad68a1e3
dawidmierzwinsk <149660396+dawidmierzwinsk@users.noreply.github.com>
2024-01-17 15:45:26 +0100

    Update 2014_04_02_193005_create_translations_table.php

--- database/migrations/2014_04_02_193005_create_translations_table.php
+++ database/migrations/2014_04_02_193005_create_translations_table.php
@@ -12,17 +12,18 @@ class CreateTranslationsTable extends Migration {
 	 */
 	public function up()
 	{
-        Schema::create('ltm_translations', function(Blueprint $table)
-        {
-	    $table->collation = 'utf8mb4_bin';
-            $table->bigIncrements('id');
-            $table->integer('status')->default(0);
-            $table->string('locale');
-            $table->string('group');
-            $table->text('key');
-            $table->text('value')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('ltm_translations')) {
+            Schema::create('ltm_translations', function(Blueprint $table) {
+    	        $table->collation = 'utf8mb4_bin';
+                $table->bigIncrements('id');
+                $table->integer('status')->default(0);
+                $table->string('locale');
+                $table->string('group');
+                $table->text('key');
+                $table->text('value')->nullable();
+                $table->timestamps();
+            });
+        }
 	}
 
 	/**
